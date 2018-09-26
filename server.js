dca841a7081738e43be5990fb53b23cd4792dadf
silentCoder404 <ashutosh@xelpmoc.in>
2018-09-26 18:06:25 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path    = require("path");
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.listen(3000);
+console.log('App is runung on port 3000');
